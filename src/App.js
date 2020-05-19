7d4afd19c6be3ab4a865df4d88955807afd04372
Shrujan Shetty <shrujan.shetty1@ibm.com>
2020-05-19 17:51:57 +0530

    finished partial code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header'; 
+import About from './Component/About Me/About';
+
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <About></About>
     </div>
   );
 }
